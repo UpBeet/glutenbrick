d59a141049fad9198a7e7865ce9d694f70bb30c1
braxton frederick <baf9201@rit.edu>
2016-03-05 16:47:07 -0500

    Setup base socket server

--- server.js
+++ server.js
@@ -1,14 +1,23 @@
 import express from 'express';
 import path from 'path';
+import SocketIO from 'socket.io';
 
 const port = process.env.PORT || 8222;
 const app = express();
 
+// sockets
+const io = SocketIO(app);
+io.on('connection', (s) => process.stdout.write('socket connected'));
+
+// static path for javascript
 app.use('/js', express.static(path.resolve(__dirname + '/build/js')));
+
+// Set routes
 app.use('/', express.static(path.resolve(__dirname + '/public')));
 app.use('/view', express.static(path.resolve(__dirname + '/public/view.html')));
 app.use('/play', express.static(path.resolve(__dirname + '/public/controller.html')));
 
+// Set port and listen
 app.set('port', port);
 app.listen(port, e => {
   if (e) throw e;
