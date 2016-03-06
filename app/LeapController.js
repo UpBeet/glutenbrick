992a433141204893fa7eb80e49d6a23187553d2b
Peter Gyory <ptg3662@rit.edu>
2016-03-06 00:57:14 -0500

    Try

--- app/LeapController.js
+++ app/LeapController.js
@@ -4,26 +4,31 @@ import * as socket from './sockets';
 import { updatePlayerTransform, closeHand, openHand } from './World';
 
 export const init = () => {
-  Leap.loop({
+  try {
+    Leap.loop({
 
-    hand: (hand) => {
-      const container = document.querySelector('#renderer');
-      const pos = hand.screenPosition();
-      const x = (pos[0] / container.scrollWidth) * 2 - 1;
-      const y = -(pos[1] / container.scrollHeight) * 2 + 1;
+      hand: (hand) => {
+        const container = document.querySelector('#renderer');
+        const pos = hand.screenPosition();
+        const x = (pos[0] / container.scrollWidth) * 2 - 1;
+        const y = -(pos[1] / container.scrollHeight) * 2 + 1;
 
-      socket.playerUpdate({
-        x,
-        y,
-        id: 'pOne',
-        roll: hand.roll(),
-        yaw: hand.yaw(),
-        pitch: hand.pitch(),
-        grabStrength: hand.grabStrength,
-      });
-    },
-  })
-  .use('screenPosition');
+        socket.playerUpdate({
+          x,
+          y,
+          id: 'pOne',
+          roll: hand.roll(),
+          yaw: hand.yaw(),
+          pitch: hand.pitch(),
+          grabStrength: hand.grabStrength,
+        });
+      },
+    })
+    .use('screenPosition');
+  }
+  catch (e) {
+    console.log(e);
+  }
 };
 
 export default {
